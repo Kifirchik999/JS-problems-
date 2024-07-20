55220b68388c47482fa88d855b95777757ed019d
Kifirchik999 <116112345+Kifirchik999@users.noreply.github.com>
2024-07-20 11:28:01 +0300

    Add files via upload

---
+++ Array_Shuffle.js
@@ -0,0 +1,16 @@
+let input = [1,2,3,4,5,6,7,8,9,10];
+
+let shuffle = function (arr) {
+    for (let i = arr.length - 1; i >0; i--) {
+        let tmp = arr[i];
+        let rnd = Math.floor(Math.random()*(i+1));
+
+        arr[i] = arr[rnd];
+        arr[rnd] = tmp;
+    }
+    return arr
+}
+
+for (let i = 0; i < input.length; i++) {
+    console.log(shuffle(input).join(','));
+}
